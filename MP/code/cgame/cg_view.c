127cb5e60da205d328d2b7f81b0645018f573165
Klaus Silveira <contact@klaussilveira.com>
2026-02-21 06:18:16 -0500

    Prevent freezes if there is no next snapshot. Use cubic decay instead of linear for prediction.

--- MP/code/cgame/cg_view.c
+++ MP/code/cgame/cg_view.c
@@ -1203,8 +1203,9 @@ static int CG_CalcViewValues( void ) {
 		float f;
 
 		t = cg.time - cg.predictedErrorTime;
-		f = ( cg_errorDecay.value - t ) / cg_errorDecay.value;
+		f = 1.0f - (float)t / cg_errorDecay.value;
 		if ( f > 0 && f < 1 ) {
+			f = f * f * f;
 			VectorMA( cg.refdef.vieworg, f, cg.predictedError, cg.refdef.vieworg );
 		} else {
 			cg.predictedErrorTime = 0;
